8a8c3c289c7e57f70e64f56f22067cd67f82a337
Mathias Andrew <mathiasnkereuwemandrew@gmail.com>
2026-04-02 01:18:24 +0100

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ['lh3.googleusercontent.com'], // Allows user profile photos from Gmail
+  },
+}
+
+module.exports = nextConfig
